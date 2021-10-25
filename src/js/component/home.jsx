b5147688e97aa2b76228d51990b1ca68eeea4630
willy5360 <willy5360@hotmail.com>
2021-10-25 19:55:20 +0000

    Adding jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,79 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import cornelita from "../../img/PinClipart.com_wheel-clipart-black-and_130263.png";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+const IMGURL = [
+	"https://images.pexels.com/photos/821365/pexels-photo-821365.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+	"https://images.pexels.com/photos/1351238/pexels-photo-1351238.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+	"https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+];
 //create your first component
+
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="jumbotron">
+			{/* <Navbar>
+				<Container>
+					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+						<Nav.Link href="#pricing">Pricing</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar> */}
+			<h1>
+				<img className="cornelita" src={cornelita} /> Mi Cornelita
+			</h1>
+			<Carousel className="Carousel_images">
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://images.pexels.com/photos/821365/pexels-photo-821365.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+						alt="First slide"
+					/>
+					<Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>
+							Nulla vitae elit libero, a pharetra augue mollis
+							interdum.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://images.pexels.com/photos/1351238/pexels-photo-1351238.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+						alt="Second slide"
+					/>
+
+					<Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://images.pexels.com/photos/3184183/pexels-photo-3184183.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+						alt="Third slide"
+					/>
+
+					<Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>
+							Praesent commodo cursus magna, vel scelerisque nisl
+							consectetur.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
 		</div>
 	);
 };
