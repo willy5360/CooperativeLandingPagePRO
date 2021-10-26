3db0964f5ce1d57a3c100186261bb55b21ca34e7
Anigabi <anag1559@gmail.com>
2021-10-26 09:19:28 +0000

    cards

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import PropTypes from "prop-types";
+
+const Cards = props => {
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Img variant="top" src={props.card_img} />
+			<Card.Body>
+				<Card.Title>{props.card_title}</Card.Title>
+				<Card.Text>{props.card_text}</Card.Text>
+				<Button variant="primary">Go somewhere</Button>
+			</Card.Body>
+		</Card>
+	);
+};
+
+{
+	Cards.propTypes = {
+		card_img: PropTypes.string,
+		card_title: PropTypes.string,
+		card_text: PropTypes.string
+	};
+}
+
+export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Card from "react-bootstrap/Card";
+
+import Cards from "../../js/component/cards.jsx";
+import image1 from "../../img/jessica-kantak-bailey--ZbSWuzMors-unsplash.jpg";
+import image2 from "../../img/courtney-cook-Mp99lbxMQOU-unsplash.jpg";
+import image3 from "../../img/shayna-douglas-H8qwryGP_h0-unsplash.jpg";
+import image4 from "../../img/visual-stories-micheile-bYMlStcT4po-unsplash.jpg";
+
+const Card_info = [
+	{
+		card_img: image1,
+		card_title: "Card Title1",
+		card_text:
+			"Some quick example text to build on the card title and make up the bulk of the cards content."
+	},
+
+	{
+		card_img: image2,
+		card_title: "Card Title2",
+		card_text:
+			"Some quick example text to build on the card title and make up the bulk of the cards content."
+	},
+
+	{
+		card_img: image3,
+		card_title: "Card Title3",
+		card_text:
+			"Some quick example text to build on the card title and make up the bulk of the cards content."
+	},
+
+	{
+		card_img: image4,
+		card_title: "Card Title4",
+		card_text:
+			"Some quick example text to build on the card title and make up the bulk of the cards content."
+	}
+];
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	let cardsContainer = Card_info.map((parameter, index) => {
+		return (
+			<Cards
+				key={index.toString()}
+				card_img={parameter.card_img}
+				card_title={parameter.card_title}
+				card_text={parameter.card_text}
+			/>
+		);
+	});
+
+	return <div>{cardsContainer}</div>;
 };
 
 export default Home;
